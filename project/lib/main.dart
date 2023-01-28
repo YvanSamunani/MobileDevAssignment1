25878a4f14b800843102ba8493fcd820d870c9cb
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-28 17:16:13 +0300

    Add icons to the cards

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TextStyles {
   TextStyle textStyle = TextStyle(
-    fontWeight: FontWeight.w500,
-    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
   );
+}
+
+class MyColors {
+  Color myColors = Color(0xFFEBF5EE);
 }
 
 void main() {
@@ -37,57 +42,132 @@ class MyHomePage extends StatelessWidget {
 class MyFoodContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          child: Card(
-            child: Text(
-              "Pizza",
-              style: TextStyles().textStyle,
-              textAlign: TextAlign.center,
+    return Container(
+      height: 600,
+      color: Color(0xff063382),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Card(
+              child: FittedBox(
+                child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Soda",
+                          style: TextStyles().textStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                        const Image(
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.contain,
+                            alignment: Alignment.bottomCenter,
+                            image: NetworkImage(
+                                'https://img.freepik.com/free-photo/mojito-drink-with-lime-lemon-mint-wood-table_1150-12269.jpg?w=740&t=st=1674906598~exp=1674907198~hmac=ebdab7d1a248181c26a0a5ee1939288be39ca1c7f7f2aaee2ad15ad56a9cc78a')),
+                        MyIcons()
+                      ],
+                    )),
+              ),
+              color: MyColors().myColors,
             ),
-            color: Color(0XFFcfcfea),
+            height: 300,
+            width: 250,
           ),
-          height: 300,
-          width: 300,
-        ),
-        Container(
-          child: Card(
-            child: Text("Salad",
-                style: TextStyles().textStyle, textAlign: TextAlign.center),
-            color: Color(0XFFcfcfea),
+          Container(
+            child: Card(
+              child: FittedBox(
+                child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Text("Salad",
+                            style: TextStyles().textStyle,
+                            textAlign: TextAlign.center),
+                        const Image(
+                          fit: BoxFit.contain,
+                          width: 200,
+                          height: 200,
+                          alignment: Alignment.bottomCenter,
+                          image: NetworkImage(
+                              'https://img.freepik.com/free-photo/greek-salad-with-fresh-tomato-cucumber-red-onion-basil-feta-cheese-black-olives-italian-herbs_2829-4480.jpg?w=740&t=st=1674907546~exp=1674908146~hmac=2223489b8a025863efc82d2cf9ecb9f83a7c213a894c614af2153f3cee9d21ca'),
+                        ),
+                        MyIcons()
+                      ],
+                    )),
+              ),
+              color: MyColors().myColors,
+            ),
+            height: 300,
+            width: 250,
           ),
-          height: 300,
-          width: 300,
-        ),
-        Container(
-          child: Card(
-              child: Text("Chicken",
-                  style: TextStyles().textStyle, textAlign: TextAlign.center),
-              color: Color(0XFFcfcfea)),
-          height: 300,
-          width: 300,
-        ),
-        Container(
-          child: Card(
-              child: Text("Soda",
-                  style: TextStyles().textStyle, textAlign: TextAlign.center),
-              color: Color(0XFFcfcfea)),
-          height: 300,
-          width: 300,
-        ),
-      ],
+          Container(
+            child: Card(
+              child: FittedBox(
+                  child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Text("Pizza",
+                        style: TextStyles().textStyle,
+                        textAlign: TextAlign.center),
+                    const Image(
+                        fit: BoxFit.contain,
+                        alignment: Alignment.bottomCenter,
+                        height: 200,
+                        width: 200,
+                        image: NetworkImage(
+                            "https://cdn.britannica.com/08/177308-050-94D9D6BE/Food-Pizza-Basil-Tomato.jpg")),
+                    MyIcons()
+                  ],
+                ),
+              )),
+              color: MyColors().myColors,
+            ),
+            height: 300,
+            width: 250,
+          ),
+        ],
+      ),
     );
     throw UnimplementedError();
   }
 }
 
-class MyFoodCard extends StatelessWidget {
+class MyIcons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [],
+    return Container(
+      width: 200,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const FaIcon(
+              FontAwesomeIcons.heart,
+              color: Color(0xffFB4D3D),
+              size: 18,
+            ),
+            const FaIcon(
+              FontAwesomeIcons.penToSquare,
+              color: Color(0xffFB4D3D),
+              size: 18,
+            ),
+            const FaIcon(
+              FontAwesomeIcons.trash,
+              color: Color(0xffFB4D3D),
+              size: 18,
+            )
+          ],
+        ),
+      ),
     );
+
     throw UnimplementedError();
   }
 }
